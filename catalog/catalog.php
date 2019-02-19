6df113e32d8f93210e22a6dc53c0c43c1468200a
m6s6 <m6s6@mail.ru>
2019-02-19 23:55:12 +0300

    Подключение файлов между собой в папке Каталог

---
+++ catalog/catalog.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/catalog/style/style.css">
+    <title>Document</title>
+</head>
+<body>
+<p class="way_site"></p>
+<h1>Название каталога</h1>
+
+<script src="/catalog/script/catalog.js"></script>
+
+</body>
+</html>
